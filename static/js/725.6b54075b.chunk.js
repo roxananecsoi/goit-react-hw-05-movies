06bff73e943e7c27b6ed165f205cc03f8b479557
roxananecsoi <146947445+roxananecsoi@users.noreply.github.com>
2024-07-24 11:59:46 +0000

    Deploying to gh-pages from @ roxananecsoi/goit-react-hw-05-movies@ffd0b3d9c81ba2f12bfef6b9ebda4eb5e94d908b 🚀

---
+++ static/js/725.6b54075b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[725],{725:function(e,t,i){i.r(t);var n=i(184);t.default=function(){return(0,n.jsx)("div",{children:"Not Found Page..."})}}}]);
+//# sourceMappingURL=725.6b54075b.chunk.js.map
